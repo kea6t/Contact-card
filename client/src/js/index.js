e13558cb3230fc1b84fd71b7f9203fab305e641b
kea6t <keasay26@gmail.com>
2022-10-04 08:47:17 -0400

    Import images for use with webpack

---
+++ client/src/js/index.js
@@ -0,0 +1,12 @@
+import "./form";
+import "./submit";
+
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener("load", function() {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bear').src = Bear;
+    document.getElementById('dog').src = Dog;
+})
