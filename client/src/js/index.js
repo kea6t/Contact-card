868da72268e396d5d746fa2e92cfe66172d62d49
kea6t <keasay26@gmail.com>
2022-10-05 18:35:12 -0400

    Render indexedDB database data on a card

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,19 +1,61 @@
-import "./form";
-import "./submit";
+// Import functions
+import {initdb, getDb, postDb, deleteDb, editDb} from './database';
+import {fetchCards} from './cards';
+import { toggleForm, clearForm } from './form';
+
+// Import CSS files
+import "../css/index.css";
+
+// Import Bootstrap
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb, getDb, postDb } from './database';
 
+// Import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// On load functionality
 window.addEventListener('load', function () {
   initdb();
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+  fetchCards();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
-// Import CSS files
-import "../css/index.css";
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+  
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+  });
