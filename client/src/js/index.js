013a5e4686cca08e22d239cecc1fca4ded2085aa
kea6t <keasay26@gmail.com>
2022-10-05 17:36:30 -0400

    adding indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,6 +9,7 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+  initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
