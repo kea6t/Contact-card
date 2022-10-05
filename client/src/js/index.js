534803abdd4a69436b988db2a6aa78e023d6abab
kea6t <keasay26@gmail.com>
2022-10-05 18:03:58 -0400

    Implement Read and Create functionality using the indexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb } from './database';
+import { initdb, getDb, postDb } from './database';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
