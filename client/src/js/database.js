83edf3470d14d05ade05da051b827a55da56ff93
kea6t <keasay26@gmail.com>
2022-10-05 17:10:48 -0400

    adding indexeddb

---
+++ client/src/js/database.js
@@ -0,0 +1,17 @@
+import { openDB } from 'idb';
+import 'regenerator-runtime/runtime';
+
+export const initdb = async () =>
+// We are creating a new database named 'contact_db' which will be using version 1 of the database.
+  openDB('contact_db', 1, {
+    // Add our database schema if it has not already been initialized.
+    upgrade(db) {
+      if (db.objectStoreNames.contains('contacts')) {
+        console.log('contacts store already exists');
+        return;
+      }
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+      db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+      console.log('contacts store created');
+    }
+  });
